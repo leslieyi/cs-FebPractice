43dd11a6f64da3d5c736d587809f9f41b584683f
leslieyi <leslieyi411@gmail.com>
2022-02-10 09:51:18 -0500

    Methods

---
+++ Feb8thMethods/Feb8thMethods/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Feb8thMethods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            //DEFENSIVE PROGRAMMING
+            //exception is just a class. we are instantiating the class
+            //if new location is null, we will never reach the next line,
+            //x and y will never change, and remain valid!
+
+            Move(newLocation.X, newLocation.Y);
+        }
+
+    }
+}
+
---
+++ Feb8thMethods/Feb8thMethods/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Feb8thMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var num = int.Parse("ABC");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Conversion of num Failed");
+
+            }
+
+            int number; //need to declare it separately, and use out, if not use the above. 
+            var result = int.TryParse("abc", out number); //this does not throw an exception. 
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion of Number Failed");
+
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }));
+
+        }
+
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+
+
+                point.Move(100, 200);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error ocurred!");
+            }
+        }
+
+
+
+    }
+}
+
